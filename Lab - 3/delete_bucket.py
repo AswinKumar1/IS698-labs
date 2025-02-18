2bd12ab2f985bb9bde20ec845f3719fb1890407e
Aswin Kumar <achu4798@gmail.com>
2025-02-17 21:53:13 -0500

    Initial commit

--- Lab - 3/delete_bucket.py
+++ Lab - 3/delete_bucket.py
@@ -1,10 +1,10 @@
-#import boto3
+import boto3
 
-#s3 = boto3.client('s3')
+s3 = boto3.client('s3')
 
 bucket_name = 'is698-lab3c'
 
-#s3.delete_bucket(Bucket=bucket_name)
+s3.delete_bucket(Bucket=bucket_name)
 
 print(f'Bucket {bucket_name} has been deleted successfully!')
 
